10541d3f682cfea44370cf78d0df4bdf9b996efd
acamberos1 <camberos.athian1@gmail.com>
2026-01-29 11:13:31 -0800

    first draft

--- src/main/java/Jotto.java
+++ src/main/java/Jotto.java
@@ -4,4 +4,52 @@
  * @Since 1/29/26
  **/
 public class Jotto {
+    private int WORD_SIZE;
+    private String currentWord;
+    private int score;
+    private ArrayList<String> playGuesses = new ArrayList<>();
+    private ArrayList<String> playWords = new ArrayList<>();
+    private String filename;
+    private ArrayList<String> wordList = new ArrayList<>();
+    private boolean debug;
+
+    public boolean pickWord(){
+
+    }
+    public String showWordList(){
+
+    }
+    public void playerGuessScores(){
+
+    }
+
+    public void setCurrentWord(){
+
+    }
+    public String readWords(){
+
+    }
+    public void play(){
+
+    }
+
+    public int guess(){
+
+    }
+    public String getPlayedWords(){
+
+    }
+    public String getCurrentWord(){
+
+    }
+    public int getLetterCount(){
+
+    }
+    public String showPlayedWords(){
+        
+    }
+    public boolean addPlayerGuess(){}
+    public void updatedWordlist(){}
+
 }
+
